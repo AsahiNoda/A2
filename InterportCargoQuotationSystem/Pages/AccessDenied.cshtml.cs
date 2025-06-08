ac058ee051fe9fbe6e58233eb6e3537517a16892
AsahiNoda <nodasy0855@gmail.com>
2025-06-08 19:04:36 +1000

    Clean ignored files from tracking

--- InterportCargoQuotationSystem/Pages/AccessDenied.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace InterportCargoQuotationSystem.Pages
-{
-    public class AccessDeniedModel : PageModel
-    {
-        public void OnGet() { }
-    }
-}
